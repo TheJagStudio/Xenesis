f4ead887265035bd3b6c9f9242571edadaeead99
TheJagStudio <81472856+TheJagStudio@users.noreply.github.com>
2023-02-19 01:21:25 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import LoginChooser from "./Screens/LoginChooser";
 import LoginEmail from "./Screens/LoginEmail";
 import PasswordRecovery from "./Screens/PasswordRecovery";
 import VerificationCode from "./Screens/VerificationCode";
+import VerificationCodePasswordRecovery from "./Screens/VerificationCodePasswordRecovery";
 import ResetPassword from "./Screens/ResetPassword";
 import Congratulation from "./Screens/Congratulation";
 import SignupEmail from "./Screens/SignupEmail";
@@ -13,6 +14,12 @@ import AccountSetup from "./Screens/AccountSetup";
 import ChooseLocation from "./Screens/ChooseLocation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Home from "./Screens/Home";
+import Explore from "./Screens/Explore";
+import Favorites from "./Screens/Favorites";
+import Tickets from "./Screens/Tickets";
+import Profile from "./Screens/Profile";
+import EventDetails from "./Screens/EventDetails";
+import Cart from "./Screens/Cart";
 
 const Stack = createNativeStackNavigator();
 
@@ -37,12 +44,19 @@ function App() {
 				<Stack.Screen name="LoginEmail" component={LoginEmail} options={{ headerShown: false }} />
 				<Stack.Screen name="PasswordRecovery" component={PasswordRecovery} options={{ headerShown: false }} />
 				<Stack.Screen name="VerificationCode" component={VerificationCode} options={{ headerShown: false }} />
+				<Stack.Screen name="VerificationCodePasswordRecovery" component={VerificationCodePasswordRecovery} options={{ headerShown: false }} />
 				<Stack.Screen name="ResetPassword" component={ResetPassword} options={{ headerShown: false }} />
 				<Stack.Screen name="Congratulation" component={Congratulation} options={{ headerShown: false }} />
 				<Stack.Screen name="SignupEmail" component={SignupEmail} options={{ headerShown: false }} />
 				<Stack.Screen name="AccountSetup" component={AccountSetup} options={{ headerShown: false }} />
 				<Stack.Screen name="ChooseLocation" component={ChooseLocation} options={{ headerShown: false }} />
 				<Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+				<Stack.Screen name="Explore" component={Explore} options={{ headerShown: false }} />
+				<Stack.Screen name="Favorites" component={Favorites} options={{ headerShown: false }} />
+				<Stack.Screen name="Tickets" component={Tickets} options={{ headerShown: false }} />
+				<Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+				<Stack.Screen name="EventDetails" component={EventDetails} options={{ headerShown: false }} />
+				<Stack.Screen name="Cart" component={Cart} options={{ headerShown: false }} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
