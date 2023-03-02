1a06bff4b46c5e3e4a8cba94f1c9c88621ada3a9
TheJagStudio <thejagstudio@gmail.com>
2023-03-02 10:36:37 +0530

    dashboard Update

--- admindashboard/views.py
+++ admindashboard/views.py
@@ -6,102 +6,130 @@ import json
 
 
 def index(request):
-    totalAmount = 0
-    totalevent = []
-    departmentArr = {}
-    eventArr = {}
-    tickets = Ticket.objects.all()
-    events = Event.objects.all()
-    userCount = Profile.objects.all().count()
-    eventCount = events.count()
-    ticketCount = tickets.count()
-    
-    for ticket in tickets:
-        event = ticket.event
-        try:
-            totalAmount = totalAmount + int((event.price).split("-")[0])
-        except:
-            pass
-        if event.department.name not in departmentArr.keys():
-            departmentArr[event.department.name] = [1, 0]
-        else:
-            departmentArr[event.department.name] = [
-                departmentArr[event.department.name][0] + 1, 0]
-        if event.name not in eventArr:
-            try:
-                eventArr[event.name] = [1, int((event.price).split("-")[0]),event.name, event.posterImage]
-            except:
-                eventArr[event.name] = [1, 0,event.name, event.posterImage]
-        else:
-            eventArr[event.name] = [eventArr[event.name][0] + 1,eventArr[event.name][1],event.name, event.posterImage]
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            totalAmount = 0
+            totalevent = []
+            departmentArr = {}
+            eventArr = {}
+            tickets = Ticket.objects.all()
+            events = Event.objects.all()
+            userCount = Profile.objects.all().count()
+            eventCount = events.count()
+            ticketCount = tickets.count()
+            
+            for ticket in tickets:
+                event = ticket.event
+                try:
+                    totalAmount = totalAmount + int((event.price).split("-")[0])
+                except:
+                    pass
+                if event.department.name not in departmentArr.keys():
+                    departmentArr[event.department.name] = [1, 0]
+                else:
+                    departmentArr[event.department.name] = [
+                        departmentArr[event.department.name][0] + 1, 0]
+                if event.name not in eventArr:
+                    try:
+                        if event.isTeamEvent == True:
+                            eventArr[event.name] = [1, int((event.teamPrice).split("-")[0]),event.name, event.posterImage]
+                        else:
+                            eventArr[event.name] = [1, int((event.price).split("-")[0]),event.name, event.posterImage]
+                    except:
+                        eventArr[event.name] = [1, int(event.price),event.name, event.posterImage]
+                else:
+                    eventArr[event.name] = [eventArr[event.name][0] + 1,eventArr[event.name][1],event.name, event.posterImage]
 
-    for event in eventArr.keys():
-        eventArr[event] = [eventArr[event][0], eventArr[event][1]*eventArr[event][0], eventArr[event][2], eventArr[event][3]]
-    eventArrValue = eventArr.values()
-    eventArrValue = sorted(eventArrValue,key=lambda x:x[1])[::-1]
-    for event in events:
-        if event.department.name not in departmentArr.keys():
-            departmentArr[event.department.name] = [0, 1]
+            for event in eventArr.keys():
+                eventpercentage = (eventArr[event][0]/ticketCount)*100
+                eventArr[event] = [eventArr[event][0], eventArr[event][1]*eventArr[event][0], eventArr[event][2], eventArr[event][3],eventpercentage]
+            eventArrValue = eventArr.values()
+            eventArrValue = sorted(eventArrValue,key=lambda x:x[1])[::-1]
+            for event in events:
+                if event.department.name not in departmentArr.keys():
+                    departmentArr[event.department.name] = [0, 1]
+                else:
+                    departmentArr[event.department.name] = [departmentArr[event.department.name][0], departmentArr[event.department.name][1]+1]
+            context = {"departmentArr": zip(departmentArr.keys(), departmentArr.values()), "eventArr":  eventArrValue[:10], "totalAmount": totalAmount , "userCount":userCount,"eventCount":eventCount,"ticketCount":ticketCount}
+            return render(request, "dashboard.html", context)
         else:
-            departmentArr[event.department.name] = [departmentArr[event.department.name][0], departmentArr[event.department.name][1]+1]
-    context = {"departmentArr": zip(departmentArr.keys(), departmentArr.values()), "eventArr":  eventArrValue[:10], "totalAmount": totalAmount , "userCount":userCount,"eventCount":eventCount,"ticketCount":ticketCount}
-    return render(request, "dashboard.html", context)
+            return render(request, "404.html")
+    return render(request, "404.html")
 
 
 def data(request):
-    departments = Department.objects.all()
-    context ={"departmentArr":departments}
-    return render(request, "admintable.html", context)
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            departments = Department.objects.all()
+            context ={"departmentArr":departments}
+            return render(request, "admintable.html", context)
+        else:
+            return render(request, "404.html")
+    return render(request, "404.html")
 
 def events(request):
-    eventArr = {}
-    tickets = Ticket.objects.all()
-    
-    for ticket in tickets:
-        event = ticket.event
-        if event.name not in eventArr:
-            try:
-                eventArr[event.name] = [1, int((event.price).split("-")[0]),event.name, event.posterImage]
-            except:
-                eventArr[event.name] = [1, 0,event.name, event.posterImage]
-        else:
-            eventArr[event.name] = [eventArr[event.name][0] + 1,eventArr[event.name][1],event.name, event.posterImage]
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            eventArr = {}
+            tickets = Ticket.objects.all()
+            ticketCount = tickets.count()
+            for ticket in tickets:
+                event = ticket.event
+                if event.name not in eventArr:
+                    try:
+                        if event.isTeamEvent == True:
+                            eventArr[event.name] = [1, int((event.teamPrice).split("-")[0]),event.name, event.posterImage]
+                        else:
+                            eventArr[event.name] = [1, int((event.price).split("-")[0]),event.name, event.posterImage]
+                    except:
+                        eventArr[event.name] = [1, int(event.price),event.name, event.posterImage]
+                else:
+                    eventArr[event.name] = [eventArr[event.name][0] + 1,eventArr[event.name][1],event.name, event.posterImage]
 
-    for event in eventArr.keys():
-        eventArr[event] = [eventArr[event][0], eventArr[event][1]*eventArr[event][0], eventArr[event][2], eventArr[event][3]]
-    eventArrValue = eventArr.values()
-    eventArrValue = sorted(eventArrValue,key=lambda x:x[1])[::-1]
-    context = {"eventArr":  eventArrValue}
-    return render(request, "admin-events-list.html", context)
+            for event in eventArr.keys():
+                eventpercentage = (eventArr[event][0]/ticketCount)*100
+                eventArr[event] = [eventArr[event][0], eventArr[event][1]*eventArr[event][0], eventArr[event][2], eventArr[event][3],eventpercentage]
+            eventArrValue = eventArr.values()
+            eventArrValue = sorted(eventArrValue,key=lambda x:x[1])[::-1]
+            context = {"eventArr":  eventArrValue}
+            return render(request, "admin-events-list.html", context)
+        else:
+            return render(request, "404.html")
+    return render(request, "404.html")
 
 def tableData(request):
-    dataType = request.GET.get("type")
-    if dataType == "event": 
-        departmentName = request.GET.get("department")
-        events = Event.objects.filter(department=Department.objects.filter(name=departmentName).first()).all()
-        eventArr = []
-        for i in events:
-            eventArr.append(i.name)
-        users = Ticket.objects.filter(event__in=events).all()
-        userArr = []
-        owner = []
-        for i in users:
-            if i.owner.user.first_name not in owner:
-                owner.append(i.owner.user.first_name)
-                userArr.append([i.owner.user.first_name,i.owner.user.username,i.owner.phone,i.owner.college])
-        return HttpResponse(json.dumps({"events":eventArr,"users":userArr}),content_type="application/json")
-    if dataType == "final":
-        eventName = request.GET.get("event")
-        users = Ticket.objects.filter(event=Event.objects.filter(name=eventName).first()).all()
-        userArr = []
-        owner = []
-        for i in users:
-            if i.owner.user.first_name not in owner:
-                owner.append(i.owner.user.first_name)
-                userArr.append([i.owner.user.first_name,i.owner.user.username,i.owner.phone,i.owner.college])
-        return HttpResponse(json.dumps({"users":userArr}),content_type="application/json")
-    else:
-        return HttpResponse(json.dumps({"data":[]}),content_type="application/json")
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            dataType = request.GET.get("type")
+            if dataType == "event": 
+                departmentName = request.GET.get("department")
+                events = Event.objects.filter(department=Department.objects.filter(name=departmentName).first()).all()
+                eventArr = []
+                for i in events:
+                    eventArr.append(i.name)
+                users = Ticket.objects.filter(event__in=events).all()
+                userArr = []
+                owner = []
+                for i in users:
+                    if i.owner.user.first_name not in owner:
+                        owner.append(i.owner.user.first_name)
+                        userArr.append([i.owner.user.first_name,i.owner.user.username,i.owner.phone,i.owner.college])
+                return HttpResponse(json.dumps({"events":eventArr,"users":userArr}),content_type="application/json")
+            if dataType == "final":
+                eventName = request.GET.get("event")
+                users = Ticket.objects.filter(event=Event.objects.filter(name=eventName).first()).all()
+                userArr = []
+                owner = []
+                for i in users:
+                    if i.owner.user.first_name not in owner:
+                        owner.append(i.owner.user.first_name)
+                        userArr.append([i.owner.user.first_name,i.owner.user.username,i.owner.phone,i.owner.college])
+                return HttpResponse(json.dumps({"users":userArr}),content_type="application/json")
+            else:
+                return HttpResponse(json.dumps({"data":[]}),content_type="application/json")
+        else:
+            return render(request, "404.html")
+    return render(request, "404.html")
 
 def singin(request):
     return HttpResponse("Admin Login")
